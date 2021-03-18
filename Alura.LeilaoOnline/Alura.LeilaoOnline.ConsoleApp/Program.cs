159489e29b4193861bf52c81d30875e759ea9a4d
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2021-03-18 09:24:55 -0300

    initial commit

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Arranje - cenário de entrada
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+            //act - método sob teste
+            leilao.TerminaPregao();
+
+            //assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+            if (valorEsperado == valorObtido)
+            {
+                Console.WriteLine("TESTE OK");
+            }
+            else
+            {
+                Console.WriteLine("TESTE FALHOU");
+            }
+        }
+    }
+}
